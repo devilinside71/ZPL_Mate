86d0e7c60b342d3e77b60c5c10f871b34b43c802
Laszlo Tamas <laszlo.devil@gmail.com>
2016-08-04 08:15:48 +0200

    New GetZPLcodeOneLine function

---
+++ ZPL_Mate/Form1.Designer.vb
@@ -0,0 +1,162 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel2 = New System.Windows.Forms.TableLayoutPanel()
+        Me.ButtonUtf2Zebra = New System.Windows.Forms.Button()
+        Me.TextBoxUTFInput = New System.Windows.Forms.TextBox()
+        Me.TextBoxZebraOutput = New System.Windows.Forms.TextBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.TableLayoutPanel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(899, 650)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.TableLayoutPanel1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(891, 624)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "UTF to ZebraCode"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(822, 546)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Calculator"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.TableLayoutPanel2, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxUTFInput, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.TextBoxZebraOutput, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(3, 3)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 3
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(885, 618)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'TableLayoutPanel2
+        '
+        Me.TableLayoutPanel2.ColumnCount = 5
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.0!))
+        Me.TableLayoutPanel2.Controls.Add(Me.ButtonUtf2Zebra, 4, 0)
+        Me.TableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel2.Location = New System.Drawing.Point(3, 559)
+        Me.TableLayoutPanel2.Name = "TableLayoutPanel2"
+        Me.TableLayoutPanel2.RowCount = 1
+        Me.TableLayoutPanel2.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel2.Size = New System.Drawing.Size(879, 56)
+        Me.TableLayoutPanel2.TabIndex = 0
+        '
+        'ButtonUtf2Zebra
+        '
+        Me.ButtonUtf2Zebra.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ButtonUtf2Zebra.Location = New System.Drawing.Point(703, 3)
+        Me.ButtonUtf2Zebra.Name = "ButtonUtf2Zebra"
+        Me.ButtonUtf2Zebra.Size = New System.Drawing.Size(173, 50)
+        Me.ButtonUtf2Zebra.TabIndex = 0
+        Me.ButtonUtf2Zebra.Text = "Generate"
+        Me.ButtonUtf2Zebra.UseVisualStyleBackColor = True
+        '
+        'TextBoxUTFInput
+        '
+        Me.TextBoxUTFInput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxUTFInput.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBoxUTFInput.Location = New System.Drawing.Point(3, 3)
+        Me.TextBoxUTFInput.Multiline = True
+        Me.TextBoxUTFInput.Name = "TextBoxUTFInput"
+        Me.TextBoxUTFInput.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxUTFInput.Size = New System.Drawing.Size(879, 272)
+        Me.TextBoxUTFInput.TabIndex = 1
+        '
+        'TextBoxZebraOutput
+        '
+        Me.TextBoxZebraOutput.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TextBoxZebraOutput.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(238, Byte))
+        Me.TextBoxZebraOutput.Location = New System.Drawing.Point(3, 281)
+        Me.TextBoxZebraOutput.Multiline = True
+        Me.TextBoxZebraOutput.Name = "TextBoxZebraOutput"
+        Me.TextBoxZebraOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxZebraOutput.Size = New System.Drawing.Size(879, 272)
+        Me.TextBoxZebraOutput.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(899, 650)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Name = "Form1"
+        Me.Text = "ZPL Mate"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
+        Me.TableLayoutPanel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents TableLayoutPanel2 As TableLayoutPanel
+    Friend WithEvents ButtonUtf2Zebra As Button
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBoxUTFInput As TextBox
+    Friend WithEvents TextBoxZebraOutput As TextBox
+End Class
---
+++ ZPL_Mate/Form1.vb
@@ -0,0 +1,81 @@
+﻿
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Me.Icon = My.Resources.ZebraTL
+        Call ZebraPrint.LoadPrinters()
+        'ComboBoxPrinter.Items.Clear()
+        'For Each printername In ZebraPrint.PrinterNames
+        '    ComboBoxPrinter.Items.Add(printername)
+        'Next
+        'ComboBoxPrinter.SelectedIndex = 0
+        Call ZebraPrint.LoadLabels()
+    End Sub
+    ''' <summary>
+    ''' Prints label.
+    ''' Replaces variables with parameters
+    ''' </summary>
+    ''' <param name="replacement_text">replacement of variables</param>
+    ''' <param name="labelsample_number">which labelsample</param>
+    Sub ZPLPrint(replacement_text As String, labelsample_number As Integer)
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+
+
+
+        'strPrinter = ZebraPrint.PrinterWinNames(ComboBoxPrinter.SelectedIndex)
+
+        strPrintText = ZebraPrint.LabelCodes(labelsample_number)
+        strPrintText = strPrintText.Replace("UTF8STR", replacement_text)
+        strPrintText = strPrintText.Replace("UTF8CODE", ZebraPrint.GetZPLutf8Code(replacement_text))
+
+        res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+
+
+    End Sub
+    ''' <summary>
+    ''' Prints label.
+    ''' Replaces variables with parameters
+    ''' </summary>
+    ''' <param name="replacement_text">replacement of variables</param>
+    ''' <param name="printer_number">which printer</param>
+    ''' <param name="labelsample_number">which labelsample</param>
+    Sub ZPLPrint(replacement_text As String, printer_number As Integer, labelsample_number As Integer)
+        Dim strPrinter As String
+        Dim strPrintText As String
+        Dim res As String
+
+
+
+        strPrinter = ZebraPrint.PrinterWinNames(printer_number)
+
+        strPrintText = ZebraPrint.LabelCodes(labelsample_number)
+        strPrintText = strPrintText.Replace("UTF8STR", replacement_text)
+        strPrintText = strPrintText.Replace("UTF8CODE", ZebraPrint.GetZPLutf8Code(replacement_text))
+
+        res = ZebraPrint.SendStringToPrinter(strPrinter, strPrintText)
+
+
+    End Sub
+    Private Sub ButtonPrint_Click(sender As Object, e As EventArgs)
+        Call ZPLPrint("Almásrétes", 0, 0)
+    End Sub
+
+    Private Sub ButtonUtf2Zebra_Click(sender As Object, e As EventArgs) Handles ButtonUtf2Zebra.Click
+        Dim strText As String
+        Dim strArray() As String
+        Dim i As Integer
+
+        strText = ZebraPrint.GetZPLutf8Code(TextBoxUTFInput.Text) & vbCrLf & vbCrLf
+        strText = strText & "http://labelary.com/viewer.html" & vbCrLf & vbCrLf
+        strText = strText & "^CWT,E:TT0003M_.FNT" & vbCrLf
+        strText = strText & "^CFT,40,30" & vbCrLf
+        strText = strText & "^CI28" & vbCrLf
+
+        strArray = Split(TextBoxUTFInput.Text, vbCrLf)
+        For i = LBound(strArray) To UBound(strArray)
+            strText = strText & "^FT10,90^FH^FD" & ZebraPrint.GetZPLutf8Code(strArray(i)) & "^FS" & vbCrLf
+        Next
+        TextBoxZebraOutput.Text = strText
+    End Sub
+End Class
